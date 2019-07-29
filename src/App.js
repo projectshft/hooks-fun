1328f0054ec38ed3128da1c2022ad457a7e6036c
amhayslip <amhayslip@gmail.com>
2019-07-29 13:01:05 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GreetingClass from './components/class-greeting';
+import GreetingFunc from './components/func-greeting';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GreetingClass />
+      {/* <GreetingFunc /> */}
     </div>
   );
 }
